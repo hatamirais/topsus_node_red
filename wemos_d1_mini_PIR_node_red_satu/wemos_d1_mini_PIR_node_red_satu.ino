c55a817394c78df0582f06c87f2ea6d955bb56b6
Hatami Bukhari <hatamierbe12@gmail.com>
2019-12-04 06:26:55 +0700

    Update wemos_d1_mini_PIR_node_red_satu.ino

--- wemos_d1_mini_PIR_node_red_satu/wemos_d1_mini_PIR_node_red_satu.ino
+++ wemos_d1_mini_PIR_node_red_satu/wemos_d1_mini_PIR_node_red_satu.ino
@@ -3,96 +3,42 @@
   Complete project details at https://randomnerdtutorials.com  
 *********/
 
-// Load libraries
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-
-// Replace with your network credentials
-const char* ssid     = "TOTOLINK_N200RE";
+// Replace with your SSID, password and MQTT broker IP address
+const char* ssid = "TOTOLINK_N200RE";
 const char* password = "hatami007";
-
-// Change the variable to your Raspberry Pi IP address, so it connects to your MQTT broker
 const char* mqtt_server = "192.168.1.2";
-// MQTT Broker IP example
+//For example
 //const char* mqtt_server = "192.168.1.144";
 
-// Initializes the espClient. You should change the espClient name if you have multiple ESPs running in your home automation system
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-// Variable to hold the temperature reading
-String temperatureString = "";
-
-// Set GPIOs for: output variable, status LED, PIR Motion Sensor, and LDR
-const int output = 15;
-const int statusLed = 12;
-const int motionSensor = A0;
-
-// Store the current output state
-String outputState = "off";
-
-// GPIO where the DS18B20 is connected to
-
-// Timers - Auxiliary variables
-unsigned long now = millis();
-unsigned long lastMeasure = 0;
-boolean startTimer = false;
-unsigned long currentTime = millis();
-unsigned long previousTime = 0; 
-
-// Don't change the function below. 
-// This function connects your ESP8266 to your router
 void setup_wifi() {
+
   delay(10);
   // We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
+
   WiFi.begin(ssid, password);
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+
+  randomSeed(micros());
+
   Serial.println("");
-  Serial.print("WiFi connected - ESP IP address: ");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
 
-// This functions is executed when some device publishes a message to a topic that your ESP8266 is subscribed to
-// Change the function below to add logic to your program, so when a device publishes a message to a topic that 
-// your ESP8266 is subscribed you can actually do something
-void callback(String topic, byte* message, unsigned int length) {
-  Serial.print("Message arrived on topic: ");
-  Serial.print(topic);
-  Serial.print(". Message: ");
-  String messageTemp;
-  
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)message[i]);
-    messageTemp += (char)message[i];
-  }
-  Serial.println();
-
-  // Feel free to add more if statements to control more GPIOs with MQTT
-  // If a message is received on the topic esp8266/output, you check if the message is either on or off. 
-  // Turns the output according to the message received
-  if(topic=="esp8266/output"){
-    Serial.print("Changing output to ");
-    if(messageTemp == "on"){
-      digitalWrite(output, LOW);
-      Serial.print("on");
-    }
-    else if(messageTemp == "off"){
-      digitalWrite(output, HIGH);
-      Serial.print("off");
-    }
-  }
-  Serial.println();
-}
-
-// This functions reconnects your ESP8266 to your MQTT broker
-// Change the function below if you want to subscribe to more topics with your ESP8266 
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
@@ -102,10 +48,7 @@ void reconnect() {
     clientId += String(random(0xffff), HEX);
     // Attempt to connect
     if (client.connect(clientId.c_str())) {
-      Serial.println("connected");  
-      // Subscribe or resubscribe to a topic
-      // You can subscribe to more topics (to control more outputs)
-      client.subscribe("esp8266/output");  
+      Serial.println("connected");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -116,48 +59,22 @@ void reconnect() {
   }
 }
 
-// Checks if motion was detected and the sensors are armed. Then, starts a timer.
-void detectsMovement() {
-  Serial.println("MOTION DETECTED!");
-  client.publish("esp8266/motion", "MOTION DETECTED!");
-  previousTime = millis();
-  startTimer = true;
-}
-
 void setup() {
-  // Start the DS18B20 sensor
-
-  // Serial port for debugging purposes
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  digitalWrite(BUILTIN_LED, HIGH);
   Serial.begin(115200);
-
-  // PIR Motion Sensor mode, then set interrupt function and RISING mode
-  pinMode(motionSensor, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);
-  
-  // Initialize the output variable and the LED as OUTPUTs
-  pinMode(output, OUTPUT);
-  pinMode(statusLed, OUTPUT);
-  digitalWrite(output, HIGH);
-  digitalWrite(statusLed, LOW);
-  
   setup_wifi();
   client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+  while (!client.connected()) {
+    reconnect();
+  }
+  Serial.print("Motion Detected");
+  // Publish MQTT message
+  String mqttMessage = "Motion Detected";  
+  client.publish("esp/pir", mqttMessage.c_str());
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
   client.loop();
-  
-  // Timer variable with current time
-  now = millis();
-
-  if ((now - previousTime > 10000) && startTimer) { 
-  client.publish("esp8266/motion", "No motion");
-  Serial.println("Motion stopped");
-  startTimer = false;
-  }
+  ESP.reset();
 }
-  // After 10 seconds have passed since motion was detected, publishes a "No motion" message
